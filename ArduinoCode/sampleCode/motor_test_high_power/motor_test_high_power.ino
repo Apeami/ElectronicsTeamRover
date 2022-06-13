2eb90cd5ba0bb80b9084eafe4b5801607464f779
Apeami <kaiduncumb@icloud.com>
2022-06-13 14:07:25 +0100

    Sample code change

--- ArduinoCode/sampleCode/motor_test_high_power/motor_test_high_power.ino
+++ ArduinoCode/sampleCode/motor_test_high_power/motor_test_high_power.ino
@@ -1,23 +1,51 @@
-void setup() {
+
+
+void runWheel(int motorSpeed, int side){
+  if (side==0){
+    if (motorSpeed==0){
+      digitalWrite(5,LOW);
+      digitalWrite(6,LOW);
+    }if (motorSpeed>0){
+      digitalWrite(5,LOW);
+      digitalWrite(6,HIGH);
+    }if (motorSpeed<0){
+      digitalWrite(5,HIGH);
+      digitalWrite(6,LOW);
+    }
+  }if (side==1){
+    if (motorSpeed==0){
+      digitalWrite(10,LOW);
+      digitalWrite(11,LOW);
+    }if (motorSpeed<0){
+      digitalWrite(10,LOW);
+      digitalWrite(11,HIGH);
+    }if (motorSpeed>0){
+      digitalWrite(10,HIGH);
+      digitalWrite(11,LOW);
+    }
+  }
+}
+
+void motorSetup(){
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
+}
 
+
+void setup() {
+  motorSetup();
 }
 
 void loop() {
-  digitalWrite(5,HIGH);
-  delay(5000);
-  digitalWrite(5,LOW);
-  digitalWrite(6,HIGH);
-  delay(5000);
-  digitalWrite(6,LOW);
-  digitalWrite(10,HIGH);
-  delay(5000);
-  digitalWrite(10,LOW);
-  digitalWrite(11,HIGH);
-  delay(5000);
-  digitalWrite(11,LOW);
-
+  runWheel(100, 0);
+  runWheel(100, 1);
+  delay(3000);
+  runWheel(0, 0);
+  runWheel(0, 1);
+  delay(3000);
+  runWheel(-100, 0);
+  runWheel(-100, 1);
+  delay(3000);
 }
